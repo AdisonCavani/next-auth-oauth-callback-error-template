357151a3ef840591ee82860f0ed312d8a1d57641
Adison Cavani <adriansrodon@tuta.io>
2023-02-27 19:20:11 +0100

    Add error reproduction

---
+++ lib/auth.ts
@@ -0,0 +1,14 @@
+import { NextAuthOptions } from "next-auth";
+import CognitoProvider from "next-auth/providers/cognito";
+
+export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+
+  providers: [
+    CognitoProvider({
+      clientId: process.env.COGNITO_CLIENT_ID!,
+      clientSecret: process.env.COGNITO_CLIENT_SECRET!,
+      issuer: process.env.COGNITO_ISSUER!,
+    }),
+  ],
+};
